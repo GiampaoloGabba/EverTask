902a7276d9d2aa34db6b7303325a5cfea673a912
Evolutionlab <info@evolutionlab.it>
2023-11-13 01:13:01 +0100

    add multitarget integration test for hostedservice

--- test/EverTask.Tests/WrokerServiceIntegrationTests.cs
+++ test/EverTask.Tests/WrokerServiceIntegrationTests.cs
@@ -5,42 +5,47 @@ namespace EverTask.Tests;
 public class WrokerServiceIntegrationTests
 {
     private readonly ITaskDispatcher _dispatcher;
-    private readonly IHostedService _workerService;
     private readonly ITaskStorage _storage;
+    private readonly IHost _host;
 
     public WrokerServiceIntegrationTests()
     {
-        var services = new ServiceCollection();
-        services.AddLogging();
-        services.AddEverTask(cfg => cfg.RegisterTasksFromAssembly(typeof(TestTaskRequest).Assembly)
-                                       .SetChannelOptions(1)).AddMemoryStorage();
-        services.AddSingleton<ITaskStorage, MemoryTaskStorage>();
+        _host = new HostBuilder()
+                .ConfigureServices((hostContext, services) =>
+                {
+                    services.AddLogging();
+                    services.AddEverTask(cfg => cfg.RegisterTasksFromAssembly(typeof(TestTaskRequest).Assembly)
+                                                   .SetChannelOptions(1)).AddMemoryStorage();
+                    services.AddSingleton<ITaskStorage, MemoryTaskStorage>();
+                })
+                .Build();
 
-        var provider = services.BuildServiceProvider();
-
-        _workerService = provider.GetRequiredService<IHostedService>();
-        _dispatcher    = provider.GetRequiredService<ITaskDispatcher>();
-        _storage       = provider.GetRequiredService<ITaskStorage>();
+        _dispatcher = _host.Services.GetRequiredService<ITaskDispatcher>();
+        _storage    = _host.Services.GetRequiredService<ITaskStorage>();
     }
 
     [Fact]
     public async Task Should_execute_task()
     {
-        await _workerService.StartAsync(CancellationToken.None);
+        await _host.StartAsync();
 
         var task = new TestTaskRequest("Test");
         await _dispatcher.Dispatch(task);
 
         await Task.Delay(500);
 
-        (await _storage.RetrievePendingTasks()).Length.ShouldBe(0);
+        var pt = await _storage.RetrievePendingTasks();
+        pt.Length.ShouldBe(0);
 
         var tasks = await _storage.GetAll();
 
         tasks.Length.ShouldBe(1);
         tasks[0].Status = QueuedTaskStatus.Completed;
 
-        await _workerService.StopAsync(CancellationToken.None);
+        var cts = new CancellationTokenSource();
+        cts.CancelAfter(2000);
+
+        await _host.StopAsync(cts.Token);
     }
 
     [Fact]
@@ -49,23 +54,22 @@ public class WrokerServiceIntegrationTests
         var task = new TestTaskRequest("Test");
         await _dispatcher.Dispatch(task);
 
-        (await _storage.RetrievePendingTasks()).Length.ShouldBe(1);
+        await _host.StartAsync();
+        await Task.Delay(500, CancellationToken.None);
 
-        await _workerService.StartAsync(CancellationToken.None);
-        await Task.Delay(500);
-        await _workerService.StopAsync(CancellationToken.None);
+        var pt = await _storage.RetrievePendingTasks();
+        pt.Length.ShouldBe(1);
 
-        await _workerService.StartAsync(CancellationToken.None);
-        await Task.Delay(500);
-        (await _storage.RetrievePendingTasks()).Length.ShouldBe(0);
+        await Task.Delay(500, CancellationToken.None);
 
         var tasks = await _storage.GetAll();
 
         tasks.Length.ShouldBe(1);
         tasks[0].Status = QueuedTaskStatus.Completed;
 
-        //await _workerService.StopAsync(CancellationToken.None);
-    }
+        var cts = new CancellationTokenSource();
+        cts.CancelAfter(2000);
 
-    
+        await _host.StopAsync(cts.Token);
+    }
 }
