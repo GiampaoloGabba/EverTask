666850be6aa2e4addebb1f9cdeca7afc454de038
Evolutionlab <info@evolutionlab.it>
2023-11-15 20:06:09 +0100

    Add a console application example

---
+++ samples/EverTask.Example.Console/Program.cs
@@ -0,0 +1,28 @@
+﻿using EverTask.Abstractions;
+using EverTask.Example.Console;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Serilog;
+
+var builder = Host.CreateDefaultBuilder(args);
+builder.ConfigureServices(services =>
+{
+    services.AddEverTask(opt =>
+            {
+                opt.SetChannelOptions(50)
+                   .SetThrowIfUnableToPersist(true)
+                   .RegisterTasksFromAssembly(typeof(Program).Assembly);
+            })
+            .AddMemoryStorage()
+            .AddSerilog(opt => opt.WriteTo.Console());
+});
+
+var host = builder.Build();
+await host.StartAsync();
+
+var dispatcher = host.Services.GetRequiredService<ITaskDispatcher>();
+
+await dispatcher.Dispatch(new SampleTaskRequest("Hello World"));
+
+await host.StopAsync();
+Console .ReadKey();
---
+++ samples/EverTask.Example.Console/SampleTask.cs
@@ -0,0 +1,15 @@
+﻿using EverTask.Abstractions;
+using Microsoft.Extensions.Logging;
+
+namespace EverTask.Example.Console;
+
+public record SampleTaskRequest(string TestProperty) : IEverTask;
+
+public class SampleTaskRequestHanlder(ILogger<SampleTaskRequestHanlder> logger) : EverTaskHandler<SampleTaskRequest>
+{
+    public override Task Handle(SampleTaskRequest backgroundTask, CancellationToken cancellationToken)
+    {
+        logger.LogInformation(backgroundTask.TestProperty);
+        return Task.CompletedTask;
+    }
+}
