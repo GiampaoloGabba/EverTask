538563bba9ac59737af63507ea638a473c8cbef9
Evolutionlab <info@evolutionlab.it>
2023-11-15 23:16:17 +0100

    disable prerelease from global.json, fix integration tests

--- test/EverTask.Tests/TestTasks.cs
+++ test/EverTask.Tests/TestTasks.cs
@@ -1,21 +1,29 @@
 ﻿namespace EverTask.Tests;
 
 public record TestTaskRequest(string Name) : IEverTask;
+
 public record TestTaskRequest2() : IEverTask;
+
 public record TestTaskRequest3() : IEverTask;
+
 public record TestTaskRequestNoHandler : IEverTask;
 
 public class TestTaskConcurrent1() : IEverTask
 {
+    public static int      Counter   { get; set; } = 0;
     public static DateTime StartTime { get; set; }
     public static DateTime EndTime   { get; set; }
 };
+
 public class TestTaskConcurrent2() : IEverTask
 {
+    public static int      Counter   { get; set; } = 0;
     public static DateTime StartTime { get; set; }
     public static DateTime EndTime   { get; set; }
 };
+
 public record TestTaskRequestNoSerializable(IPAddress notSerializable) : IEverTask;
+
 public record ThrowStorageError() : IEverTask;
 
 public class TestTaskHanlder : EverTaskHandler<TestTaskRequest>
@@ -42,9 +50,9 @@ public class TestTaskHanlder2 : EverTaskHandler<TestTaskRequest2>
     }
 }
 
-public class TestTaskHanlder3 : EverTaskHandler<TestTaskRequest2>
+public class TestTaskHanlder3 : EverTaskHandler<TestTaskRequest3>
 {
-    public override Task Handle(TestTaskRequest2 backgroundTask, CancellationToken cancellationToken)
+    public override Task Handle(TestTaskRequest3 backgroundTask, CancellationToken cancellationToken)
     {
         return Task.CompletedTask;
     }
@@ -52,27 +60,23 @@ public class TestTaskHanlder3 : EverTaskHandler<TestTaskRequest2>
 
 public class TestTaskConcurrent1Handler : EverTaskHandler<TestTaskConcurrent1>
 {
-    public override Task Handle(TestTaskConcurrent1 backgroundTask, CancellationToken cancellationToken)
+    public override async Task Handle(TestTaskConcurrent1 backgroundTask, CancellationToken cancellationToken)
     {
         TestTaskConcurrent1.StartTime = DateTime.Now;
-        Task.Delay(500, cancellationToken).Wait(cancellationToken);
-        var file = File.Create("test1.txt");
-        file.Dispose();
+        await Task.Delay(1000, cancellationToken);
+        TestTaskConcurrent1.Counter = 1;
         TestTaskConcurrent1.EndTime = DateTime.Now;
-        return Task.CompletedTask;
     }
 }
 
 public class TestTaskConcurrent2Handler : EverTaskHandler<TestTaskConcurrent2>
 {
-    public override Task Handle(TestTaskConcurrent2 backgroundTask, CancellationToken cancellationToken)
+    public override async Task Handle(TestTaskConcurrent2 backgroundTask, CancellationToken cancellationToken)
     {
         TestTaskConcurrent2.StartTime = DateTime.Now;
-        Task.Delay(500, cancellationToken).Wait(cancellationToken);
-        var file = File.Create("test2.txt");
-        file.Dispose();
+        await Task.Delay(1000, cancellationToken);
+        TestTaskConcurrent2.Counter = 1;
         TestTaskConcurrent2.EndTime = DateTime.Now;
-        return Task.CompletedTask;
     }
 }
 
@@ -101,4 +105,3 @@ class TestInternalTaskHanlder : EverTaskHandler<InternalTestTaskRequest>
         return Task.CompletedTask;
     }
 }
-
