c800cbac57060cd4da733ed44a6ca02b7f4a119b
Evolutionlab <info@evolutionlab.it>
2023-11-22 03:06:57 +0100

    HourSchedulerBuilder

---
+++ src/EverTask/Scheduler/Recurring/Builder/HourSchedulerBuilder.cs
@@ -0,0 +1,17 @@
+﻿namespace EverTask.Scheduler.Recurring.Builder;
+
+public class HourSchedulerBuilder(RecurringTask task) : IHourSchedulerBuilder
+{
+    public IMinuteSchedulerBuilder AtMinute(int minute)
+    {
+        ArgumentNullException.ThrowIfNull(task.HourInterval);
+
+        if (minute is < 0 or > 59)
+            throw new ArgumentOutOfRangeException(nameof(minute));
+
+        task.HourInterval.OnMinute = minute;
+        return new MinuteSchedulerBuilder(task);
+    }
+
+    public void MaxRuns(int maxRuns) =>task.MaxRuns = maxRuns;
+}
