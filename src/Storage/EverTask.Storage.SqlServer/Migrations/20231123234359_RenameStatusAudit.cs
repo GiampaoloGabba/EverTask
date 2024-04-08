e8de20b622462592e8faa64dd36f90f14204a625
Evolutionlab <info@evolutionlab.it>
2024-04-09 01:59:07 +0200

    Refactor schema reference in RenameStatusAudit migration
    The existing schema reference "EverTask" is replaced with a dynamic reference through _dbContext.Schema. This change also introduces a dependency injection for ITaskStoreDbContext in the constructor of this class. Now, the schema can be changed across all methods in RenameStatusAudit class merely by changing the value of _dbContext.Schema.

--- src/Storage/EverTask.Storage.SqlServer/Migrations/20231123234359_RenameStatusAudit.cs
+++ src/Storage/EverTask.Storage.SqlServer/Migrations/20231123234359_RenameStatusAudit.cs
@@ -7,40 +7,49 @@ namespace EverTask.Storage.SqlServer.Migrations
     /// <inheritdoc />
     public partial class RenameStatusAudit : Migration
     {
+        private readonly ITaskStoreDbContext _dbContext;
+
+        public RenameStatusAudit(ITaskStoreDbContext dbContext)
+        {
+            _dbContext = dbContext ?? throw new ArgumentNullException(nameof(dbContext));
+        }
+
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
         {
+            TargetModel.GetDefaultSchema();
+
             migrationBuilder.DropForeignKey(
                 name: "FK_QueuedTaskStatusAudit_QueuedTasks_QueuedTaskId",
-                schema: "EverTask",
+                schema: _dbContext.Schema,
                 table: "QueuedTaskStatusAudit");
 
             migrationBuilder.DropPrimaryKey(
                 name: "PK_QueuedTaskStatusAudit",
-                schema: "EverTask",
+                schema: _dbContext.Schema,
                 table: "QueuedTaskStatusAudit");
 
             migrationBuilder.RenameTable(
                 name: "QueuedTaskStatusAudit",
-                schema: "EverTask",
+                schema: _dbContext.Schema,
                 newName: "StatusAudit",
                 newSchema: "EverTask");
 
             migrationBuilder.RenameIndex(
                 name: "IX_QueuedTaskStatusAudit_QueuedTaskId",
-                schema: "EverTask",
+                schema: _dbContext.Schema,
                 table: "StatusAudit",
                 newName: "IX_StatusAudit_QueuedTaskId");
 
             migrationBuilder.AddPrimaryKey(
                 name: "PK_StatusAudit",
-                schema: "EverTask",
+                schema: _dbContext.Schema,
                 table: "StatusAudit",
                 column: "Id");
 
             migrationBuilder.AddForeignKey(
                 name: "FK_StatusAudit_QueuedTasks_QueuedTaskId",
-                schema: "EverTask",
+                schema: _dbContext.Schema,
                 table: "StatusAudit",
                 column: "QueuedTaskId",
                 principalSchema: "EverTask",
@@ -54,35 +63,35 @@ namespace EverTask.Storage.SqlServer.Migrations
         {
             migrationBuilder.DropForeignKey(
                 name: "FK_StatusAudit_QueuedTasks_QueuedTaskId",
-                schema: "EverTask",
+                schema: _dbContext.Schema,
                 table: "StatusAudit");
 
             migrationBuilder.DropPrimaryKey(
                 name: "PK_StatusAudit",
-                schema: "EverTask",
+                schema: _dbContext.Schema,
                 table: "StatusAudit");
 
             migrationBuilder.RenameTable(
                 name: "StatusAudit",
-                schema: "EverTask",
+                schema: _dbContext.Schema,
                 newName: "QueuedTaskStatusAudit",
                 newSchema: "EverTask");
 
             migrationBuilder.RenameIndex(
                 name: "IX_StatusAudit_QueuedTaskId",
-                schema: "EverTask",
+                schema: _dbContext.Schema,
                 table: "QueuedTaskStatusAudit",
                 newName: "IX_QueuedTaskStatusAudit_QueuedTaskId");
 
             migrationBuilder.AddPrimaryKey(
                 name: "PK_QueuedTaskStatusAudit",
-                schema: "EverTask",
+                schema: _dbContext.Schema,
                 table: "QueuedTaskStatusAudit",
                 column: "Id");
 
             migrationBuilder.AddForeignKey(
                 name: "FK_QueuedTaskStatusAudit_QueuedTasks_QueuedTaskId",
-                schema: "EverTask",
+                schema: _dbContext.Schema,
                 table: "QueuedTaskStatusAudit",
                 column: "QueuedTaskId",
                 principalSchema: "EverTask",
